54d85fa019f822ff4721b64093c1865573196c04
Trevor Gray <trevormgray@gmail.com>
2025-04-10 15:31:18 -0400

    Number disable

--- javascript/components/pageSwitcher.jsx
+++ javascript/components/pageSwitcher.jsx
@@ -55,7 +55,7 @@ const PageSwitcher = ({ current_page, total_pages, onPageChange }) => {
                 <button
                   className="page-link"
                   onClick={() => onPageChange(number)}
-                  disabled={number === Number(current_page)}
+                  disabled={number === Number(current_page + 1)}
                 >
                   {number}
                 </button>
