96cb4bf48ee2e26a890ccb13e123fbd3bbdf74f7
Trevor Gray <trevormgray@gmail.com>
2025-04-09 23:03:31 -0400

    scroll

--- javascript/components/search-page.jsx
+++ javascript/components/search-page.jsx
@@ -102,6 +102,7 @@ const SearchPage = () => {
     }
     };
   const handlePageChange = (newPageNumber) => {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // Add this line
     setSearchParams({ q: searchTerm, page: newPageNumber + 1 }); // 1-based for URL
     fetchResults(searchTerm, newPageNumber); // 0-based for API
     };
