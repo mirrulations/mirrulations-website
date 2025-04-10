fc190a997f7953272f9e71b211bca91c7707dffd
Trevor Gray <trevormgray@gmail.com>
2025-04-10 13:12:47 -0400

    testing arrows

--- javascript/components/pageSwitcher.jsx
+++ javascript/components/pageSwitcher.jsx
@@ -23,8 +23,8 @@ const PageSwitcher = ({ current_page, total_pages, onPageChange }) => {
   }
 
   const arrowButtons = [
-    { text: "<<", page: 1, disabled: current_page === 1 },
-    { text: "<", page: current_page - 1, disabled: current_page === 1 },
+    { text: "<<", page: 1, disabled: current_page === 0 },
+    { text: "<", page: current_page - 1, disabled: current_page === 0 },
     { text: ">", page: current_page + 1, disabled: current_page >= maxPagesToShow },
     { text: ">>", page: maxPagesToShow, disabled: current_page === maxPagesToShow },
   ];
