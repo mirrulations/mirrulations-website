a500b2debb17f515b42d06a9048be89e8f2f2ae7
Davin Glynn <glynnd@moravian.edu>
2025-04-16 13:31:50 -0400

    used isOpenForComment field

--- javascript/components/results.jsx
+++ javascript/components/results.jsx
@@ -42,12 +42,7 @@ const ResultsSection = ({ results, onPageChange }) => {
               <p><strong>Summary:</strong> {docket.summary ? (docket.summary.length > 300 ? `${docket.summary.substring(0, 300)}...` : docket.summary) : "No summary available"}</p>
               {/* Use the new TimelineModal component instead of displaying dates directly */}
               <TimelineModal key={docket.id} timelineDates={docket.timelineDates}/>
-              <p>
-                <strong>Open for comments?:</strong>{" "}
-                {docket.timelineDates.dateCommentsOpened !== null && docket.timelineDates.dateClosed === null
-                  ? "Yes"
-                  : "No"}
-              </p>
+              <p><strong>Open for comments?:</strong> {docket.isOpenForComment === true ? "Yes" : "No"}</p>
             </div>
             
             <div className="d-flex align-items-end">
